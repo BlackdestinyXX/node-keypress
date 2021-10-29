09a7e7cd05a7f2bb99a5a8fb88d1ba8b842c30a6
Bl4ckdestinyXX <blackdestinyx145@gmail.com>
2021-10-29 16:08:44 +0200

    Added VSC intellisense

--- typings/index.ts
+++ typings/index.ts
@@ -12,8 +12,17 @@ process.stdin.on('keypress', (str, key) => {
     keyPressCallback(key)
 });
 
+/**
+ * @description Main class of the library
+ * @author Bl4ckdestinyXX
+ **/
 export default class NodeKeyboard {
 
+    /**
+     * @description Main class of the library
+     * @author Bl4ckdestinyXX
+     * @param { Function } => Callback when a key is pressed
+    **/
     onKeypress(callback: any): void {
         keyPressCallback = callback;
     }
