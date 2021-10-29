801dddc333c00c73aa59a3cfc539f0ea97213db0
Bl4ckdestinyXX <blackdestinyx145@gmail.com>
2021-10-29 16:32:12 +0200

    Removed useless files

--- typings/test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var index_1 = require("./index");
-var keylistener = new index_1["default"]();
-keylistener.onKeypress(function (key) {
-    console.log(key);
-});
